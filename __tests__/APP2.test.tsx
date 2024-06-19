70bd195d0f4728900c72b0f2a1e73589444b363b
Aleh Kuis <pestleralex@gmail.com>
2024-06-19 17:03:48 +0300

    fix: fix test

---
+++ __tests__/APP2.test.tsx
@@ -0,0 +1,10 @@
+import { describe, it } from 'vitest';
+import { shallow } from 'enzyme';
+import '../test-setup/setup';
+import  App  from '../src/App';
+
+describe('App', () => {
+  it('Should render the App correctly', () => {
+    shallow(<App />);
+  });
+});
--- __tests__/name-surname-validation.test.tsx
+++ __tests__/name-surname-validation.test.tsx
@@ -6,9 +6,9 @@ describe('Function nameValidation works correctly', () => {
   test('expect correct returned value from nameValidation', () => {
     expect(nameValidation()).toStrictEqual({
       pattern: {
-        value: /^(?=.*[A-Z])(?=.*[a-z]).{2,}/,
+        value: /^[А-ЯЁ][а-яё]+$/,
         message:
-          'Имя должно быть на английском языке, начинаться с большой буквы и содержать не менее 2 символов',
+          'Имя должно содержать только кириллические символы и начинаться с большой буквы',
       },
       required: {
         value: true,
@@ -19,9 +19,9 @@ describe('Function nameValidation works correctly', () => {
   test('expect correct returned value from nameValidation', () => {
     expect(surnameValidation()).toStrictEqual({
       pattern: {
-        value: /^(?=.*[A-Z])(?=.*[a-z]).{2,}/,
+        value: /^[А-ЯЁ][а-яё]+$/,
         message:
-          'Фамилия должна быть на английском языке, начинаться с большой буквы и содержать не менее 2 символов',
+          'Фамилия должна содержать только кириллические символы, начинаться с большой буквы и содержать не менее двух символов',
       },
       required: {
         value: true,
