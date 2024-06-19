c14146e2d528ec5d1d5f50f0176c380021d12c92
Aleh Kuis <pestleralex@gmail.com>
2024-06-19 17:31:36 +0300

    feat: change test App

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,11 +2,16 @@ import { render, screen } from '@testing-library/react';
 
 import App from '../src/App';
 import { describe, it } from 'vitest';
+import { shallow } from 'enzyme';
 
 describe('App', () => {
-  it('renders headline', () => {
+  it('renders App screen', () => {
     render(<App />);
     screen.debug();    
   });
+  it('Should render the App correctly', () => {
+    shallow(<App />);
+  });
 });
+
 
